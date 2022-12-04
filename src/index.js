f06dae40fd3e649af1de6f9534395ad359affd66
nafernandez <nafernandez@theinit.com>
2022-12-04 14:24:14 +0100

    if-else-statements

--- src/index.js
+++ src/index.js
@@ -2,3 +2,25 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
+
+// -----
+const condition = true;
+
+if (condition) {
+  console.log('Yes');
+} else {
+  console.log('No');
+}
+
+const number = 99;
+let result; // undefined
+
+if (number === 1) {
+  result = 'One';
+} else if (number === 99) {
+  result = 'Ninety-Nine'
+} else {
+  result = 'No Match'
+}
+
+console.log(result)
